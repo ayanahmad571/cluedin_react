dbefd46f0bad0a974bc67e1e5d8159dbc1a40f2b
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2023-11-17 19:08:45 +0000

    Profile Changes now work

--- page_helper/AboutUsBody.tsx
+++ page_helper/AboutUsBody.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Linking} from 'react-native';
+import {CLUEDIN_DARK_SCHEME} from '../utils/constants';
 
 const AboutUsBody = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Welcome to CluedIn</Text>
       <Text style={styles.description}>
-        Discover the world of CluedIn, where knowledge and fun meet every day. Our app brings you a brand-new question daily at 2pm GMT, offering a chance to test your wits and learn something new.
+        Discover the world of CluedIn, where knowledge and fun meet every day.
+        Our app brings you a brand-new question daily at 2pm GMT, offering a
+        chance to test your wits and learn something new.
       </Text>
       <Text style={styles.description}>
-        With monthly themes that keep things exciting and a leaderboard reset on the first day of each month, CluedIn is the ultimate destination for trivia enthusiasts. Join us now and Clue In to the thrill of daily challenges!
+        With monthly themes that keep things exciting and a leaderboard reset on
+        the first day of each month, CluedIn is the ultimate destination for
+        trivia enthusiasts. Join us now and Clue In to the thrill of daily
+        challenges!
       </Text>
-      
+
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => Linking.openURL('https://cluedin.me/privacy-policy')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => Linking.openURL('https://cluedin.me/privacy-policy')}>
           <Text style={styles.buttonText}>Privacy Policy</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => Linking.openURL('https://cluedin.me/terms-and-conditions')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            Linking.openURL('https://cluedin.me/terms-and-conditions')
+          }>
           <Text style={styles.buttonText}>Terms and Conditions</Text>
         </TouchableOpacity>
       </View>
@@ -27,7 +39,7 @@ const AboutUsBody = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: CLUEDIN_DARK_SCHEME.background,
     padding: 20,
   },
   header: {
--- page_helper/ProfileBody.tsx
+++ page_helper/ProfileBody.tsx
@@ -5,10 +5,15 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 import AuthContext from '../utils/AuthContext';
 import {jwtDecode} from 'jwt-decode';
 import {decode} from 'base-64';
+import LoaderOverlay from './LoaderOverlay';
+import {API_BASE_URL} from '../utils/constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 global.atob = decode;
 
 const UserProfileBody = () => {
@@ -17,7 +22,8 @@ const UserProfileBody = () => {
   const [user_jwt, setUserJwt] = useState({});
   const [isUsernameChanged, setIsUsernameChanged] = useState(false);
   const [isSaveButtonActive, setIsSaveButtonActive] = useState(false);
-  const {user} = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const {user, setUser} = useContext(AuthContext);
 
   useEffect(() => {
     const decodeUsername = () => {
@@ -40,9 +46,99 @@ const UserProfileBody = () => {
     setIsSaveButtonActive(username !== username_initial);
   }, [username]);
 
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.setItem('JWT_USER', '');
+
+      const jwtUser = await AsyncStorage.getItem('JWT_USER');
+      // Navigate to the EnterOtp.tsx screen (make sure to import the necessary modules for navigation)
+
+      setTimeout(() => {
+        // Code to be executed after a 2-second delay
+        setUser('');
+      }, 2000);
+    } catch (error) {
+      console.log('Error whilst deleting storage');
+    }
+    // Perform any additional logout logic here
+  };
+
+  const getUsernameApi = async () => {
+    try {
+      const requestBody = `new_username=${username}`;
+
+      const response = await fetch(`${API_BASE_URL}/update_username`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${user}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: requestBody,
+      });
+
+      if (response.status === 200) {
+        // Successfully submitted the answer, update the question data
+        Alert.alert(
+          'Success',
+          'Username has been updated, you will now be logged out.',
+        );
+        handleLogout();
+        setIsLoading(false);
+      } else if (response.status === 406) {
+        Alert.alert(
+          'Error',
+          'Username is not unique, please chose another one :)',
+        );
+        setIsLoading(false);
+      } else {
+        // Handle the error cases as needed
+        Alert.alert('Error', 'Failed to update username, contact site admin.');
+        setIsLoading(false);
+      }
+    } catch (error) {
+      Alert.alert(
+        'Error',
+        'Your network is unstable, please try again at a later time',
+      );
+      setIsLoading(false);
+    }
+  };
+  const validateUsername = username => {
+    // Validation 1: Number of characters must be between 3 and 15 (inclusive)
+    if (!(username.length >= 3 && username.length <= 15)) {
+      Alert.alert('Error', 'Number of characters must be between 3 and 15');
+      return false;
+    }
+
+    // Validation 2: Only numbers and alphabets allowed
+    if (!/^[a-zA-Z0-9]+$/.test(username)) {
+      Alert.alert('Error', 'Only numbers and alphabets allowed');
+      return false;
+    }
+
+    // Validation 3: No spaces allowed in the username
+    if (/\s/.test(username)) {
+      Alert.alert('Error', 'No spaces allowed in the username');
+      return false;
+    }
+
+    return true;
+  };
+
   const handleSave = () => {
+    setIsLoading(true);
     console.log('Username saved:', username);
     console.log(user_jwt);
+
+    // Send a request to your API using the provided id
+    // For demonstration, we'll simulate an API response after 2 seconds
+    if (validateUsername(username)) {
+      setTimeout(() => {
+        getUsernameApi();
+      }, 2000);
+    } else {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -92,9 +188,10 @@ const UserProfileBody = () => {
           isSaveButtonActive ? styles.activeSaveButton : {},
         ]}
         onPress={handleSave}
-        disabled={!isSaveButtonActive}>
+        disabled={!isSaveButtonActive && !isLoading}>
         <Text style={styles.saveButtonText}>Save</Text>
       </TouchableOpacity>
+      <LoaderOverlay visible={isLoading} />
     </View>
   );
 };
