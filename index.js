3a033da291c3409870f0e1f7245a9b26d7e9b52f
Ayan Ahmad <ayan@Ayans-Mac-mini-2.lan>
2023-12-12 18:40:25 +0000

    IOS Fix NOTIF

--- index.js
+++ index.js
@@ -1,44 +1,85 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform} from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import BackgroundTimer from 'react-native-background-timer';
 import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import BackgroundTimer from 'react-native-background-timer';
 
-// Create a notification channel
-PushNotification.createChannel(
-  {
-    channelId: 'default-channel-id', // Provide a unique channel ID
-    channelName: 'Default Channel',
-    channelDescription: 'A default channel for app notifications',
-    playSound: true,
-    soundName: 'default',
-    importance: 4, // Importance level (4 means high importance)
+// Register the main component
+AppRegistry.registerComponent(appName, () => App);
+
+const os = Platform.OS; // 'ios' or 'android'
+
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
   },
-  created => console.log(`Channel created: ${created}`)
-);
+
+  onNotification: function (notification) {
+    // console.log("NOTIFICATION:", notification);
+    // Remove badge number when notification is opened
+    PushNotification.setApplicationIconBadgeNumber(0);
+
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+    PushNotification.removeAllDeliveredNotifications();
+  },
+
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios'
+});
+
+
+if(os === 'android'){
+  // Create a notification channel
+  PushNotification.createChannel(
+    {
+      channelId: 'default-channel-id',
+      channelName: 'Default Channel',
+      channelDescription: 'A default channel for app notifications',
+      playSound: true,
+      soundName: 'default',
+      importance: 4,
+    },
+    // created => console.log(`Channel created: ${created}`)
+  );
+}
 
 // Function to check if the current time is 2pm UTC
 const isTwoPMUTC = () => {
   const now = new Date();
-  return now.getUTCHours() === 14 && now.getUTCMinutes() === 0;
+  const hours_req = 18;
+  const mins_req = 38;
+  const checker = (now.getUTCHours() === hours_req && now.getUTCMinutes() === mins_req)
+  return checker;
 };
 
 // Function to be executed by the timer
 const timerFunction = () => {
-  console.log('Timer executed at:', new Date());
+  // console.log('Timer executed at:', new Date());
 
   // Check if the current time is 2pm UTC
   if (isTwoPMUTC()) {
-    // Send a push notification with the created channel ID
+    const msg_notif = {
+      "title": 'CluedIn Trivia',
+      "body": 'Your daily trivia is ready, open the app to answer today\'s question and see what others are scoring!'
+    }
     PushNotification.localNotification({
-      channelId: 'default-channel-id', // Specify the channel ID here
-      title: 'CluedIn Trivia',
-      message: 'Your daily trivia is ready, open the app to answer today\'s question and see what others are scoring!',
+      channelId: 'default-channel-id',
+      title: msg_notif.title,
+      message: msg_notif.body,
+      sound: 'default',
+      number: 1,
+      
     });
   }
 };
 
 // Start the timer with a 1-minute interval
 BackgroundTimer.runBackgroundTimer(timerFunction, 60000);
-
-AppRegistry.registerComponent(appName, () => App);
